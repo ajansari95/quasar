535865a748b4c8cd413503d6ec485ab14729296a
LaurensKubat <32776056+LaurensKubat@users.noreply.github.com>
2023-06-09 14:26:52 +0200

    add multihop router

--- smart-contracts/contracts/index-collateral/src/error.rs
+++ smart-contracts/contracts/index-collateral/src/error.rs
@@ -1,6 +1,8 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
 
+use multihop_router::ContractError as RouterError;
+
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
@@ -8,6 +10,7 @@ pub enum ContractError {
 
     #[error("Unauthorized")]
     Unauthorized {},
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+
+    #[error("{0}")]
+    Router(#[from] RouterError),
 }
